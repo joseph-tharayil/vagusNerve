0708572ae62c7f8aaca6c42466d5199403fe88cf
Tharayil Joseph <joseph.tharayil@epfl.ch>
2024-07-29 21:58:21 +0200

    Updates simulation launch files

--- simulations/.ipynb_checkpoints/analytic-Standoff-sideways-highConductivity-monopolar-far-checkpoint.py
+++ simulations/.ipynb_checkpoints/analytic-Standoff-sideways-highConductivity-monopolar-far-checkpoint.py
@@ -2,28 +2,33 @@ import numpy as np
 import pandas as pd
 
 
-from scipy.stats import norm
-from scipy.io import loadmat
-
-from scipy.optimize import leastsq
-from scipy.optimize import least_squares
-from scipy.io import loadmat
-from scipy.interpolate import interp1d
-from scipy.stats import norm
-import multiprocessing as mp
-from scipy.fft import fft, ifft, fftshift,ifftshift
-from scipy.signal import fftconvolve, butter, sosfilt
-
-from scipy.stats import rv_histogram
-from mpi4py import MPI
-
-from math import gamma
-
-from scipy.optimize import curve_fit
-
-import quantities as pq
-
 import sys
+
+from vagusNerve.runSim import runSim
+
+def main(outputfolder, distanceIdx):
+
+    stimulus = {'current':np.arange(100,510,100)/28.6,
+                'stimulusDirectory':{
+                    "myelinated":'/gpfs/bbp.cscs.ch/project/proj85/scratch/vagusNerve/Data/TitrationGoodConductivity_Standoff_Sideways_HighConductivity.xlsx',
+                    "unmyelinated":'/gpfs/bbp.cscs.ch/project/proj85/scratch/vagusNerve/Data/TitrationGoodConductivity_Standoff_Sideways_Unmyelinated_HighConductivity.xlsx'
+                }
+               }
+
+    recording = {'recordingCurrent':514e-6,
+                 'recordingDirectory':'/gpfs/bbp.cscs.ch/project/proj85/scratch/vagusNerve/Data/PhiConductivity_Monopolar_Far_Corrected/'
+            }
+
+    runSim(outputfolder, distanceIdx, stimulus, recording)
+
+
+if __name__=="__main__":
+    
+    
+    outputfolder = sys.argv[1]
+    distanceIdx = int(sys.argv[2])
+    
+    main(outputfolder,distanceIdx)
 
     
 def main(outputfolder,distanceIdx):
